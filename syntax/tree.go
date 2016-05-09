a372de7b9b81dcef0420ec8750a4a563b9819f8b
刘阳 <numbksco@gmail.com>
2016-05-09 17:27:35 +0800

    Finished syntax module.

---
+++ syntax/tree.go
@@ -0,0 +1,78 @@
+package syntax
+
+import "github.com/ksco/slua/scanner"
+
+type SyntaxTree interface{}
+
+type (
+	Chunk struct {
+		Block SyntaxTree
+	}
+
+	Block struct {
+		Stmts []SyntaxTree
+	}
+
+	DoStatement struct {
+		Block SyntaxTree
+	}
+
+	WhileStatement struct {
+		Exp   SyntaxTree
+		Block SyntaxTree
+	}
+
+	IfStatement struct {
+		Exp         SyntaxTree
+		TrueBranch  SyntaxTree
+		FalseBranch SyntaxTree
+	}
+
+	ElseifStatement struct {
+		Exp         SyntaxTree
+		TrueBranch  SyntaxTree
+		FalseBranch SyntaxTree
+	}
+
+	ElseStatement struct {
+		Block SyntaxTree
+	}
+
+	LocalNameListStatement struct {
+		NameList  SyntaxTree
+		ExpList   SyntaxTree
+		NameCount int
+	}
+
+	AssignmentStatement struct {
+		VarList SyntaxTree
+		ExpList SyntaxTree
+	}
+
+	VarList struct {
+		VarList []SyntaxTree
+	}
+
+	Terminator struct {
+		Tok *scanner.Token
+	}
+
+	BinaryExpression struct {
+		Left    SyntaxTree
+		Right   SyntaxTree
+		OpToken *scanner.Token
+	}
+
+	UnaryExpression struct {
+		Exp     SyntaxTree
+		OpToken *scanner.Token
+	}
+
+	NameList struct {
+		Names []*scanner.Token
+	}
+
+	ExpressionList struct {
+		ExpList []SyntaxTree
+	}
+)
